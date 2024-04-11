cfe4d36a055eb4964da16687e993e84498a83dc3
FacuTombesi <facundomtombesi@gmail.com>
2024-04-10 21:21:43 -0300

    admin dashboard finished

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,11 +8,16 @@ import {
 import db from "@/db/db";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
 
+function wait(duration: number) {
+  return new Promise(resolve => setTimeout(resolve, duration));
+}
+
 async function getSalesData() {
   const data = await db.order.aggregate({
     _sum: { pricePaidInCents: true },
     _count: true
   })
+  await wait(2000)
 
   return {
     amount: (data._sum.pricePaidInCents || 0) / 100,
@@ -20,10 +25,38 @@ async function getSalesData() {
   };
 }
 
-async function getUserData() {}
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true }
+    })
+  ])
+
+  return {
+    userCount,
+    averageValuePerUser: userCount === 0
+      ? 0
+      : (orderData._sum.pricePaidInCents || 0) / userCount / 100
+  };
+}
+
+async function getProductData() {
+  const [activeProducts, inactiveProducts] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } })
+  ])
+
+  return { activeProducts, inactiveProducts };
+}
 
 export default async function AdminDashboard() {
-  const salesData = await getSalesData()
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData()
+  ])
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
@@ -33,8 +66,13 @@ export default async function AdminDashboard() {
       />
       <DashboardCard
         title="Customers"
-        subtitle={`${formatCurrency(345)} average value`}
-        body={formatNumber(4547)}
+        subtitle={`${formatCurrency(userData.averageValuePerUser)} average value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active products"
+        subtitle={`${formatNumber(productData.inactiveProducts)} inactive`}
+        body={formatNumber(productData.activeProducts)}
       />
     </div>
   );
