1aab8b2cceabc855dbb2800410552eee627f8450
FacuTombesi <facundomtombesi@gmail.com>
2024-04-19 18:42:39 -0300

    credentials updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,13 @@ async function isAuthenticated(req: NextRequest) {
 
   const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":")
 
-  return username === process.env.ADMIN_USERNAME && (await isValidPassword(password, process.env.ADMIN_HASHED_PASSWORD as string));
+  return (
+    username === process.env.ADMIN_USERNAME &&
+    (await isValidPassword(
+      password,
+      process.env.ADMIN_HASHED_PASSWORD as string
+    ))
+  );
 }
 
 export const config = {
