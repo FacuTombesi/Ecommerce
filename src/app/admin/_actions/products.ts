2a3e5dedaa30020a62c12a74d9ede79b5eb552aa
FacuTombesi <facundomtombesi@gmail.com>
2024-04-24 18:00:11 -0300

    cache added

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -4,6 +4,7 @@ import db from "@/db/db";
 import { z } from "zod";
 import fs from "fs/promises";
 import { notFound, redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 const fileSchema = z.instanceof(File, { message: "Required" })
 const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
@@ -44,11 +45,17 @@ export async function addProduct(prevState: unknown, formData: FormData) {
     }
   })
 
+  revalidatePath("/")
+  revalidatePath("/products")
+
   redirect("/admin/products");
 }
 
 export async function toggleProductAvailability(id: string, isAvailableForPurchase: boolean) {
   await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
+
+  revalidatePath("/")
+  revalidatePath("/products")
 }
 
 export async function deleteProduct(id: string) {
@@ -58,6 +65,9 @@ export async function deleteProduct(id: string) {
 
   await fs.unlink(product.filePath)
   await fs.unlink(`public${product.imagePath}`)
+
+  revalidatePath("/")
+  revalidatePath("/products")
 }
 
 const editSchema = addSchema.extend({
@@ -101,6 +111,9 @@ export async function updateProduct(id: string, prevState: unknown, formData: Fo
       imagePath
     }
   })
+
+  revalidatePath("/")
+  revalidatePath("/products")
 
   redirect("/admin/products");
 }
