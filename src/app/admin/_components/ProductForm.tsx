01c343760fe36d6ec2fc5944fbb7be701bd6c45e
FacuTombesi <facundomtombesi@gmail.com>
2025-01-09 20:10:16 -0300

    update(+): updated next.js 15.1.3 to 15.1.4 & fixed product form bugs

--- src/app/admin/_components/ProductForm.tsx
+++ src/app/admin/_components/ProductForm.tsx
@@ -5,24 +5,20 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
-import { useState } from "react";
+import { useActionState, useState } from "react";
 import { addProduct } from "../_actions/products";
 import { useFormStatus } from "react-dom";
 
 export function ProductForm() {
-  const [priceInCents, setPriceInCents] = useState<number>();
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    await addProduct(formData);
-  }
+  const [error, action] = useActionState(addProduct, {});
+  const [priceInCents, setPriceInCents] = useState<string>("");
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <form action={action} className="space-y-4">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" placeholder="Add product name" required />
+        <Input type="text" id="name" name="name" placeholder="Add product name" required></Input>
+        {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="priceInCents">Price in cents</Label>
@@ -33,21 +29,25 @@ export function ProductForm() {
           placeholder="Add product price in cents. Example: $16.50 = 1650 cents"
           required
           value={priceInCents}
-          onChange={e => setPriceInCents(Number(e.target.value) || undefined)}
-        />
-        <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
+          onChange={e => setPriceInCents(e.target.value)}
+        ></Input>
+        <div className="text-muted-foreground">{formatCurrency(Number(priceInCents || 0) / 100)}</div>
+        {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" placeholder="Add product description" required />
+        <Textarea id="description" name="description" placeholder="Add product description" required></Textarea>
+        {error.description && <div className="text-destructive">{error.description}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required></Input>
+        {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input type="file" id="image" name="image" required></Input>
+        {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
     </form>
