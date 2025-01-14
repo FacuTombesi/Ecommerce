2eaea19e59bfbcdf81cd1021aeb0071feba4583f
FacuTombesi <facundomtombesi@gmail.com>
2025-01-14 19:14:26 -0300

    add(+): added new home page and basic titles

---
+++ src/app/(customerPages)/page.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { Product } from "@prisma/client";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+function getNewestProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase : true },
+    orderBy: { createdAt: "desc"} ,
+    take: 6,
+  });
+}
+
+function getPopularProducts() {
+  return db.product.findMany({
+    where: { isAvailableForPurchase : true },
+    orderBy: { orders: { _count: "desc" } },
+    take: 6,
+  });
+}
+
+export default function HomePage() {
+  return (
+    <main className="space-y-12">
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+      <ProductGridSection title="Popular" productsFetcher={getPopularProducts} />
+    </main>
+  );
+}
+
+type ProductsGridSectionProps = {
+  title: string
+  productsFetcher: () => Promise<Product[]>
+}
+
+function ProductGridSection({ title, productsFetcher }: ProductsGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link href="/products" className="space-x-2">
+            <span>View all</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
+      </div>
+      <div className="grid grid-col-1 md:grid-col-2 lg:grid-col-3 gap-6">
+
+      </div>
+    </div>
+  );
+}
